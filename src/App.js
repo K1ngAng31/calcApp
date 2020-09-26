ebb0ca316b49feb4d73a1f16d1a68047d41af7b9
K1ngAng31 <angelsantanahernandez2@gmail.com>
2020-09-26 15:13:49 -0600

    calculator app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(props){
+    super(props)//calls the constructor of the parent class
+    this.state = {
+      display: "0", // this will show 0
+      equation:"" // this will handle the input equation
+    }
+    // create our inputs to use
+    //use our bind method to pass the context of this
+    this.numberInput = this.numberInput.bind(this);
+    this.operationInput = this.operationInput.bind(this);
+    this.decimalInput = this.decimalInput.bind(this);
+    this.clearInput = this.clearInput.bind(this);
+    this.calculate = this.calculate.bind(this);
+  }
+
+  //Now we create our function
+
+
 }
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
